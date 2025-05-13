c15cf0514fb9e65adfa2cf43161c08b9c7c6001a
Korkogor <korkogor@gmail.com>
2025-05-13 19:35:52 +0300

    Added more clickable elements.

--- ExperimentWithControls/MainWindow.xaml.cs
+++ ExperimentWithControls/MainWindow.xaml.cs
@@ -19,6 +19,17 @@ namespace ExperimentWithControls
         public MainWindow()
         {
             InitializeComponent();
+
+        }
+
+        private void numberTextBox_TextChanged(object sender, TextChangedEventArgs e)
+        {
+            number.Text = numberTextBox.Text;
+        }
+
+        private void numberTextBox_PreviewTextInput(object sender, TextCompositionEventArgs e)
+        {
+            e.Handled = !int.TryParse(e.Text, out int result);
         }
     }
 }
